e305f213e4cf60175a984cefe71e82d2e930d7d6
Himaaghna <87596357+Himaaghna@users.noreply.github.com>
2022-10-06 17:50:52 +0530

    Add files via upload

---
+++ p115.py
@@ -0,0 +1,7 @@
+import os
+import shutil
+
+source="C:/Users/dibak/OneDrive/Desktop/Python/class-111/test.txt"
+destination="C:/Users/dibak/OneDrive/Desktop/Python/class-111/sampleFolder"
+
+dest=shutil.copy(source,destination)
